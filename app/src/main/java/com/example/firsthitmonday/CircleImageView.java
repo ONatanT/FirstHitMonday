c7f69271714d6334c4a1cd0c2e80242ae94c07ab
Orel-Natan <O.T.Natan@gmail.com>
2024-02-11 14:33:00 +0200

    Pull all the data and pictures

--- app/src/main/java/com/example/firsthitmonday/CircleImageView.java
+++ app/src/main/java/com/example/firsthitmonday/CircleImageView.java
@@ -1,4 +1,5 @@
 package com.example.firsthitmonday;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Path;
@@ -7,27 +8,17 @@ import android.util.AttributeSet;
 
 public class CircleImageView extends androidx.appcompat.widget.AppCompatImageView {
 
-        public static float radius = 18.0f;
-        public static float margin = 0.0f;
+    public CircleImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-        public CircleImageView(Context context) {
-            super(context);
-        }
-
-        public CircleImageView(Context context, AttributeSet attrs) {
-            super(context, attrs);
-        }
-
-        public CircleImageView(Context context, AttributeSet attrs, int defStyle) {
-            super(context, attrs, defStyle);
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-            Path clipPath = new Path();
-            RectF rect = new RectF(margin, margin, this.getWidth()-margin, this.getHeight()-margin);
-            clipPath.addRoundRect(rect, radius, radius, Path.Direction.CW);
-            canvas.clipPath(clipPath);
-            super.onDraw(canvas);
-        }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Path path = new Path();
+        int width = this.getWidth();
+        int height = this.getHeight();
+        path.addOval(new RectF(0, 0, width, height), Path.Direction.CW);
+        canvas.clipPath(path);
+        super.onDraw(canvas);
+    }
 }
