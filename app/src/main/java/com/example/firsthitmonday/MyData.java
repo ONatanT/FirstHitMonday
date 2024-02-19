fab9fb62a57fc8d7508dc90c6b56bef456f3ae32
FARCHYMU7 <matan.farchy@gmail.com>
2024-02-19 21:05:35 +0200

    add title and remove dark item

--- app/src/main/java/com/example/firsthitmonday/MyData.java
+++ app/src/main/java/com/example/firsthitmonday/MyData.java
@@ -4,10 +4,10 @@ package com.example.firsthitmonday;
 
 public class MyData {
 
-    static String[] nameArray = {"Dark", "Jonas", "marta", "Noah", "Hannah", "Ulrich", "Mikkel","Bartosh", "Alex", "Charlotte", "Helge"};
-    static String[] textArray = {"Netflix tv show", "The main character", "Jonas' girlfriend", "The scary priest who kidnapped children", "Jonas's mother", "Marta's father, cheats on his wife with Jonas' mother", "The son of ulrich and marta's brother, was kidnapped by the priest", "Jonas's best friend", "Bartosh's father and the manager of the nuclear power plant", "The director of the police station who investigates the abduction of the children","A mysterious old man whom everyone suspects of kidnapping the children"};
+    static String[] nameArray = {"Jonas", "marta", "Noah", "Hannah", "Ulrich", "Mikkel","Bartosh", "Alex", "Charlotte", "Helge"};
+    static String[] textArray = {"The main character", "Jonas' girlfriend", "The scary priest who kidnapped children", "Jonas's mother", "Marta's father, cheats on his wife with Jonas' mother", "The son of ulrich and marta's brother, was kidnapped by the priest", "Jonas's best friend", "Bartosh's father and the manager of the nuclear power plant", "The director of the police station who investigates the abduction of the children","A mysterious old man whom everyone suspects of kidnapping the children"};
 
-    static Integer[] drawableArray = {R.drawable.dark, R.drawable.jonas, R.drawable.marta,
+    static Integer[] drawableArray = {R.drawable.jonas, R.drawable.marta,
             R.drawable.noah, R.drawable.hannah, R.drawable.ulrich, R.drawable.mikkel,
             R.drawable.bartosh, R.drawable.alex, R.drawable.charlotte,R.drawable.helge};
 
